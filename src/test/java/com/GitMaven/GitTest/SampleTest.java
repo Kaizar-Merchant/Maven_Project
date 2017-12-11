23fa253966796cb2ce8733ecd3f286df637ee2c0
kzr.merchant <kzr_merchant@yahoo.com>
2017-12-11 21:57:06 +0530

    adding maven repo

---
+++ src/test/java/com/GitMaven/GitTest/SampleTest.java
@@ -0,0 +1,9 @@
+package com.GitMaven.GitTest;
+
+public class SampleTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello java");
+	}
+
+}
---
+++ src/test/java/com/GitMaven/GitTest/TestTestNG.java
@@ -0,0 +1,26 @@
+package com.GitMaven.GitTest;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TestTestNG {
+	@BeforeMethod
+	public void Setup()
+	{
+		System.out.println("Tihs is before Method");
+	}
+	@Test(description = "Test1")
+	public void Test1()
+	{
+		System.out.println("This is Test1");
+	}
+	
+	@Test(description = "Test2")
+	public void Test2()
+	{
+		System.out.println("This is Test1");
+		Assert.assertEquals("Kaizar", "kaizar");
+	}
+
+}
